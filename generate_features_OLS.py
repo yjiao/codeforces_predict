bd04dd93b930690a55df89e6fb4c7eaee175bdcb
yjiao <joy20123@gmail.com>
2017-06-12 22:51:21 -0400

    feature eng for OLS

--- generate_features_OLS.py
+++ generate_features_OLS.py
@@ -1,16 +1,27 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import psycopg2
-from collections import defaultdict
-from os.path import exists
 
-# current to-do:
-# keep those with only 1 contests?
-# keep last trailing activities?
+# ---------------------------------------------
+# some hard-coded groupings of sparse features
+# languages
+java = [ 'Java 6', 'Java 8', 'Java 8 ZIP', 'Java 7' ]
+python = [ 'Python 3', 'Python 2', 'PyPy 2', 'PyPy 3']
+lowlevel = [ 'GNU C++14', 'GNU C', 'MS C++', 'GNU C++', 'GNU C++0x', 'MS C#', 'GNU C11', 'GNU C++11 ZIP', 'GNU C++11', 'Mono C#', 'Delphi', ]
+otherlang = [ 'Haskell', 'Factor', 'Picat', 'Secret_171', 'PHP', 'Tcl', 'Scala', 'Io', 'FPC', 'J', 'Rust', 'JavaScript', 'Ada', 'Go', 'Cobol', 'Befunge', 'R oco', 'Ruby', 'Kotlin', 'F#', 'Perl', 'Pike', 'D', 'Ocaml' ]
+# verdicts 
+errors = [ "COMPILATION_ERROR", "RUNTIME_ERROR", "CRASHED", "REJECTED", "IDLENESS_LIMIT_EXCEEDED"]
+wrong = [ "TIME_LIMIT_EXCEEDED", "WRONG_ANSWER", "CHALLENGED", "MEMORY_LIMIT_EXCEEDED" ]
+# practice mdoes
+practice = [ 'GYM', 'OUT_OF_COMPETITION', 'VIRTUAL', 'PRACTICE' ]
+# columns that are too sparse or reduandant to be informative
+drop = [ 'FALSE', 'SKIPPED', 'TESTING', 'PARTIAL', 'REJECTED', 'PRESENTATION_ERROR', 'FAILED', 'rank' ]
 
-def get_categorical_variables( colnames ):
-    con = psycopg2.connect(database='codeforces', user='Joy')
-    cur = con.cursor()
+# ---------------------------------------------
+# constants
+secinday = 3600.0*24.0 # days
+
+def get_categorical_variables( colnames, cur):                                                                                                              
     catvars = []
     for c in colnames:
         cur.execute("select * from %s" % c, con)
@@ -18,191 +29,155 @@ def get_categorical_variables( colnames ):
 
     return catvars
 
-def getTraining(user, user_rating, problem_rating, hdl_cid_pid_list, binvars, con, df_tags):
-    filename = 'rnn_train/%s.csv' % user
-    print filename
-    trainlist = []
-    if len(user_rating.shape) == 1:
-        print "     Not enough contests for user", user_rating.shape
-        return
-    user_rating.is_copy = False
-    user_rating.reset_index(inplace=True)
+def summarize(qfront, qback, subdict, prevcontest, nextcontest, dfrat, df_prate, df_tags, month=3):
+    df = pd.DataFrame.from_dict(subdict[qfront:qback+1])
     
-    for hdl_cid_pid in hdl_cid_pid_list:
-        if hdl_cid_pid[1] not in ['497', '366', '429']:
-            continue
-        q = """
-        SELECT * FROM submissions
-            WHERE
-                handle = '%s'
-                AND
-                contestid = '%s'
-                AND
-                problemid = '%s'
-        """ % (hdl_cid_pid[0], hdl_cid_pid[1], hdl_cid_pid[2])
-
-        df_user_problem = pd.read_sql(q, con)
-        df_user_problem.is_copy = False
-
+    # --------------------------------------------------
+    # per-problem features
+    gprob = df.groupby(['contestid', 'problemid'])
+    for k, v in gprob:
+        cid = k[0]
+        pid = k[1]
         ex = dict()
-        
         # generic problem info
-        ex['points'] = df_user_problem.points[0]
-        ex['problemid'] = df_user_problem.problemid.values[0]
-        ex['contestid'] = df_user_problem.contestid.values[0]
-        
+        ex['points'] = v.points.values[0]
+        ex['problemid'] = v.problemid.values[0]
+        ex['contestid'] = v.contestid.values[0]
+
         # ----------------------------------
         # user rating info
         # find closest PREVIOUS contest
         # if there is no next contest,then skip this entry
-        ex['starttimeseconds'] = min(df_user_problem.starttimeseconds)
-        ex['stoptimeseconds'] = max(df_user_problem.starttimeseconds)
-
-        # all contests greater than submit time
-        idx_prevcontest = user_rating.ratingupdatetimeseconds <= ex['starttimeseconds']
-        idx_nextcontest = user_rating.ratingupdatetimeseconds >= ex['stoptimeseconds']
+        ex['starttimeseconds'] = min(v.starttimeseconds)
+        ex['stoptimeseconds'] = max(v.starttimeseconds)
         
-        # if this is the first few submissions before the user has competed,
-        # then we set the "current rating" to the next contest rating instead
-        if not np.any(idx_prevcontest):
-            user_rating_contest = user_rating.loc[idx_nextcontest]
-            # take min of all contests greater than submit time--this is the next contest
-            idx_nextcontest = user_rating_contest.ratingupdatetimeseconds == min(user_rating_contest.ratingupdatetimeseconds)
-            user_rating_contest = user_rating_contest.loc[idx_nextcontest].to_dict(orient='records')[0]
-        else:
-            user_rating_contest = user_rating.loc[idx_nextcontest]
-            idx_prevcontest = user_rating_contest.ratingupdatetimeseconds == max(user_rating_contest.ratingupdatetimeseconds)
-            user_rating_contest = user_rating_contest.loc[idx_prevcontest].to_dict(orient='records')[0]
-
-        user_rating_contest['next_contestid'] = user_rating_contest['contestid']
-        user_rating_contest.pop('contestid', None)
-        user_rating_contest.pop('index', None)
-        ex.update(user_rating_contest)
-
         # ----------------------------------
         # verdicts
-        vcnt = df_user_problem.verdict.value_counts()
+        vcnt = v.verdict.value_counts()
         vdict = vcnt.to_dict()
         ex.update(vdict)
 
+        # ----------------------------------
         # participant type
-        pcnt = df_user_problem.participanttype.value_counts()
+        pcnt = v.participanttype.value_counts()
         pdict = pcnt.to_dict()
         for t in pdict.iterkeys():
             ex[t] = 1
-
-        # ----------------------------------
-        # language
-        lcnt = df_user_problem.language.value_counts()
-        ldict = lcnt.to_dict()
-        ex.update(ldict)
-
+            
         # ----------------------------------
         # problem rating
-        if (hdl_cid_pid[1], hdl_cid_pid[2]) in problem_rating.index:
-            ex['problem_rating'] = problem_rating.loc[hdl_cid_pid[1], hdl_cid_pid[2]].values[0]
+        if (cid, pid) in df_prate.index:
+            ex['problem_rating'] = df_prate.loc[cid, pid].values[0]
         else:
             ex['problem_rating'] = -1
-
+            
         # ----------------------------------
         # time to solves
-        # Get the differences in times of interest
-        # Otherwise normalization would make them too small to be meaningful
-
-        # current quantities
-        # ratingupdatetimeseconds    time at end of a contest
-        # solvetimeseconds           time at first solve
-        # starttimeseconds           time of first submit
-        # stoptimeseconds            time of last submit
-
-        # quantities of interest
-        # hours_solve_to_contest      hours between solve and end of contest
-        # hours_submit_to_contest     hours between first submit and end of contest
-        # hours_submit_to_solve       hours between first submit and solve
-        solvetime = df_user_problem.loc[df_user_problem.verdict=='OK', 'starttimeseconds']
-
-        ex['hours_submit_to_contest'] = (ex['ratingupdatetimeseconds'] - ex['starttimeseconds']) / 3600.0
-
+        solvetime = v.loc[v.verdict=='OK', 'starttimeseconds']
         if len(solvetime) > 0:
             ex['solvetimeseconds'] = min(solvetime)
-            ex['hours_solve_to_contest'] = (ex['ratingupdatetimeseconds'] - ex['solvetimeseconds']) / 3600.0
             ex['hours_submit_to_solve'] = (ex['solvetimeseconds'] - ex['starttimeseconds']) / 3600.0
         else:
             # some problems were never solved
-            ex['solvetimeseconds'] = -1
-            ex['hours_solve_to_contest'] = -1
             ex['hours_submit_to_solve'] = -1
-        trainlist.append(ex)
+        
+        # -----------------------------------------------------------------------
+        # tags
+        idx_tag = (cid, pid)
+        if (idx_tag) in df_tags:
+            for t in df_tags.loc[idx_tag]['tag'].values:
+                ex[t] = 1
+    
+        print ex
 
-        print df_user_problem[['problemid', 'contestid']]
-        print ex['problemid'], ex['contestid']
+    smoothratinglabel = "smoothed_%dmonths" % month
+    print dfrat.loc[prevcontest]['contestid'], dfrat.loc[nextcontest]['contestid']
 
-    df_train = pd.DataFrame.from_dict(trainlist)
-    print df_train[['contestid', 'problemid']]
-    for t in binvars:
-        if t not in df_train.columns:
-            df_train[t] = np.nan
-            
-    df_train.to_csv(filename, mode='w', index=False, header=True)
+def parse_user(handle, con, df_smooth, df_prate, df_tags):
+    # --------------------------
+    # per user
+    dfsub = pd.read_sql("select * from submissions where handle='%s'" % handle, con)
+    dfsub.is_copy = False
+    dfsub['type'] = 'problem'
+
+    dfrat = df_smooth.loc[df_smooth.handle == handle]
+    dfrat.is_copy = False
+
+    dfsub.sort_values('starttimeseconds', inplace=True)
+    dfrat.sort_values('starttimeseconds', inplace=True)
+    dfsub.reset_index(inplace=True)
+    dfrat.reset_index(inplace=True)
+
+    dfsub['index'] = dfsub.index
+    dfrat['index'] = dfrat.index
+
+    subdict = dfsub.to_dict(orient='records')
+    ratdict = dfrat.to_dict(orient='records')
+
+    idx = dfsub[['starttimeseconds', 'type', 'index']]
+    idx = idx.to_dict(orient="records")
+    idx_rat = dfrat[['starttimeseconds', 'type', 'index']]
+    idx_rat = idx_rat.to_dict(orient="records")
+
+    idx.extend(idx_rat)
+    idx.sort(key = lambda x: x['starttimeseconds'])
+
+    # note we are using a list as a queue since the python Queue class has heavy overhead
+    # including locks, which we do not need
+    qfront = 0
+    qback = 0
+    cutoff = 30
+    prevcontest = -1
+
+    for i in idx:
+        if i['type'] == 'problem':
+            qback += 1
+        else:
+            assert(i['type'] == 'contest')
+            # found contest
+            curtime = i['starttimeseconds']
+            days_elapsed = (curtime - subdict[qfront]['starttimeseconds']) / secinday
+            # remove problems that occurred too long ago
+            while days_elapsed > cutoff:
+                qfront += 1
+                days_elapsed = (curtime - subdict[qfront]['starttimeseconds']) / secinday
+            # process previous problems solved
+            #print ratdict[i['index']]['contestid'], "-------------"
+            if prevcontest == -1:
+                summarize(qfront, qback, subdict, i['index'], i['index'], dfrat, df_prate, df_tags)
+            else:
+                summarize(qfront, qback, subdict, prevcontest, i['index'], dfrat, df_prate, df_tags)
+            prevcontest = i['index']
 
 if __name__ == "__main__":
     con = psycopg2.connect(database='codeforces', user='Joy')
-    cur = con.cursor()
 
-    # note this is 4x faster than getting it from sql
+    # smoothed user ratings
     df_smooth = pd.read_csv('user_ratings_smoothed.csv', engine = 'c')
-
-    cur.execute("SELECT * FROM handles")
-    all_handles = [h[0] for h in cur.fetchall()]
-
-    with open('handle_cid_pid_keys.txt') as f:
-        keys = [line.strip() for line in f.readlines()]
-
-    binvars = get_categorical_variables([
-        'all_participanttypes',
-        'all_tags',
-        'all_verdicts',
-        'all_language'
-    ])
-
-    # ------------------------------------------
-    # problem stats
-    # ------------------------------------------
-    # problem rating and tags
-    df_prate = pd.read_sql("SELECT * FROM problem_rating", con)
-    df_prate.set_index(['contestid', 'problemid'], inplace=True)
-
-    df_tags = pd.read_sql("SELECT * FROM tags", con)
-    df_tags.set_index(['contestid', 'problemid'], inplace=True)
-
-    df_smooth.reset_index(inplace=True)
-    df_smooth.set_index(['handle'], inplace=True)
+    df_smooth['type'] = 'contest'
+    df_smooth['starttimeseconds'] = df_smooth['ratingupdatetimeseconds']
     df_smooth.drop('contestname', axis=1, inplace=True)
     df_smooth.drop('time', axis=1, inplace=True)
 
+    # problem ratings
+    df_prate = pd.read_sql("SELECT * FROM problem_rating", con)                                                                                                 
+    df_prate.set_index(['contestid', 'problemid'], inplace=True)
 
-    # ------------------------------------------
-    # dictionary of [user] -> [contestid, problemid]
-    user_dict = defaultdict(list)
-    keys = [k.split(',') for k in keys]
-    for k in keys:
-        user_dict[ k[0] ].append(k)
+    # problem tags
+    df_tags = pd.read_sql("SELECT * FROM tags", con)
+    df_tags.set_index(['contestid', 'problemid'], inplace=True)
+    df_tags.sort_index(inplace=True)
 
-
+    cur.execute("SELECT * FROM handles")
+    all_handles = [h[0] for h in cur.fetchall()]
     present_handles = set(df_smooth.index)
 
+    lastidx = 0
+    for i, user in enumerate(all_handles[lastidx:]):
+        if user in present_handles:
+            print lastidx + i, user
+            user_rating = df_smooth.loc[user, :]
+            parse_user(user, con, df_tags)
 
-    cnt = 0
 
-
-    lastidx = 57
-    user = 'chenmark'
-    user_rating = df_smooth.loc[user, :]
-    getTraining(user, user_rating, df_prate, user_dict[user], binvars, con, df_tags)
-#    for i, user in enumerate(all_handles[lastidx:]):
-#        if user in present_handles:
-#            print lastidx + i, user
-#            user_rating = df_smooth.loc[user, :]
-#            getTraining(user, user_rating, df_prate, user_dict[user], binvars, con)
-#
 
