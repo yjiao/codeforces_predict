463ed14051595cedcee315b320aa33ced2ca472a
yjiao <joy20123@gmail.com>
2017-06-16 20:31:20 -0400

    added function for getting X and Y in ols_functions.py for ensemble learning

--- ols_functions.py
+++ ols_functions.py
@@ -71,6 +71,66 @@ def get_train_val_test():
 
     return X, Y
 
+# note that this version is for ensemble learning, and we are not using the smoothed version of delta
+def get_data_for_ensemble():
+    files = glob('ols_train/*.csv')
+    alldata = []
+    for h in files:
+        try:
+            t = pd.read_csv(h, engine='c')
+            alldata.append(t)
+        except:
+            pass
+    alldata = pd.concat(alldata)
+
+    # --------------------------------------
+    # get previous features
+    prev_train = pd.read_csv('training_linear_regression_smooth.csv', engine='c')
+    df_smooth = pd.read_csv('user_ratings_smoothed_atleast5contests.csv', engine='c')
+
+    mergedata = alldata
+    mergedata.set_index(['handle', 'contestid'], inplace=True)
+    prev_train.set_index(['handle', 'contestid'], inplace=True)
+    df_smooth.set_index(['handle', 'contestid'], inplace=True)
+    mergedata = mergedata.join(prev_train)
+
+    # --------------------------------------
+    # remove first contests for all users
+    # there is no delta for here
+    g = mergedata.groupby('handle')
+    glist = []
+    for k, v in g:
+        glist.append(v.drop(v.index[v.starttimeseconds == min(v.starttimeseconds)], axis=0))
+    mergedata = pd.concat(glist)
+
+    X = mergedata
+    Y = X['newrating'] - X['oldrating']
+    dropcols = [
+                'delta_smoothed_1months',
+                'delta_smoothed_2months',
+                'delta_smoothed_3months',
+                'delta_smoothed_4months',
+                'delta_smoothed_5months',
+                'contestid',
+                'type',
+                'ratingupdatetimeseconds',
+                'starttimeseconds',
+                'oldrating',
+                "level_0",
+                'index',
+                'rating_change_smooth',
+                'rating_change',
+                'user_rating_smooth',
+                'contesttime',
+                'min_probability_practice',
+                'min_probability_contest'
+              ]
+    for d in dropcols:
+        if d in X.columns:
+            X.drop(d, inplace=True, axis=1)
+
+    return X, Y
+
 def drafts():
     # --------------------------------------
     # Clip values that are arbitrarily large
